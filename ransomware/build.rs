80569327b0a35f83932277b79d3ad94a81f87bd2
20221219 <d.coello.luna@student.tue.nl>
2026-01-15 18:57:08 +0100

    adding static linking for windows gui lib

--- ransomware/build.rs
+++ ransomware/build.rs
@@ -2,5 +2,8 @@ fn main() {
     // Required for libsodium RNG on Windows GNU
     println!("cargo:rustc-link-lib=advapi32");
     println!("cargo:rustc-link-lib=bcrypt");
+    
+    // Embed Windows manifest for Common Controls v6
+    embed_resource::compile("app.rc", embed_resource::NONE);
 }
  
